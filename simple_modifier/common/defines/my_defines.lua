f0d0e7b23de44858adab48fbc20ee5ffa9b8b318
LinRN <linruneng@foxmail.com>
2023-12-16 19:44:03 +0800

    增加Define/贸易中心文件

---
+++ simple_modifier/common/defines/my_defines.lua
@@ -0,0 +1,4 @@
+NDefines.NCountry.CULTURE_MIN_DEVELOPMENT_TO_PROMOTE = 1	-- 接受文化所需的最小发展度
+NDefines.NCountry.NAT_FOCUS_YEARS = 1						-- 改变国家焦点的冷却时间（year）
+NDefines.NCountry.REVOLT_SIZE_BASE = 0                      -- 基础叛军
+NDefines.NCountry.TRIBE_STARTING_DEV = 0				    -- 最低部落发展度
