0492f30c84a73cd971161dbaff676e3dd8587462
LinRN <linruneng@foxmail.com>
2023-12-23 22:14:02 +0800

    增加宗教相关事件

--- simple_modifier/common/defines/my_defines.lua
+++ simple_modifier/common/defines/my_defines.lua
@@ -2,3 +2,6 @@ NDefines.NCountry.CULTURE_MIN_DEVELOPMENT_TO_PROMOTE = 1	-- 接受文化所需�
 NDefines.NCountry.NAT_FOCUS_YEARS = 1						-- 改变国家焦点的冷却时间（year）
 NDefines.NCountry.REVOLT_SIZE_BASE = 0                      -- 基础叛军
 NDefines.NCountry.TRIBE_STARTING_DEV = 0				    -- 最低部落发展度
+
+NDefines.NReligion.MIN_HARMONIZE_DEVELOPMENT = 1            -- 和谐宗教所需的最小发展度
+NDefines.NReligion.YEARLY_HARMONIZATION_PROGRESS = 100      -- 每年和谐进度
