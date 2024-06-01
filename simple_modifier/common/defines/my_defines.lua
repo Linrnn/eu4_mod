f6fffac2e197f63d6e59f0e7ef5358636a43a2ca
LinRN <linruneng@foxmail.com>
2024-06-01 15:53:58 +0800

    适配1.37

--- simple_modifier/common/defines/my_defines.lua
+++ simple_modifier/common/defines/my_defines.lua
@@ -1,15 +1,15 @@
 NDefines.NCountry.CULTURE_MIN_DEVELOPMENT_TO_PROMOTE = 1	            -- 接受文化所需的最小发展度
 NDefines.NCountry.NAT_FOCUS_YEARS = 1						            -- 改变国家焦点的冷却时间（年）
 NDefines.NCountry.REVOLT_SIZE_BASE = 0                                  -- 基础叛军
-NDefines.NCountry.CONCENTRATE_DEVELOPMENT_DEVELOPMENT_DECREASE = 0      -- 集中发展度概率
-NDefines.NCountry.PILLAGE_CAPITAL_DEVELOPMENT_DECREASE = 0              -- 掠夺首都概率
-NDefines.NCountry.RAZE_PROVINCE_COOLDOWN_YRS = 0                        -- 剥削发展度冷却时间（年）
-NDefines.NCountry.EXPLOIT_COOLDOWN_MONTHS = 0                           -- 游牧烧地（月）
+--NDefines.NCountry.CONCENTRATE_DEVELOPMENT_DEVELOPMENT_DECREASE = 0      -- 集中发展度概率
+--NDefines.NCountry.PILLAGE_CAPITAL_DEVELOPMENT_DECREASE = 0              -- 掠夺首都概率
+--NDefines.NCountry.RAZE_PROVINCE_COOLDOWN_YRS = 0                        -- 剥削发展度冷却时间（年）
+--NDefines.NCountry.EXPLOIT_COOLDOWN_MONTHS = 0                           -- 游牧烧地（月）
 --NDefines.NCountry.TRIBE_STARTING_DEV = 0				                -- 最低部落发展度
 
-NDefines.NMilitary.ARMY_DRILL_MAX_MORALE = 2							-- 陆军演习最大士气
+--NDefines.NMilitary.ARMY_DRILL_MAX_MORALE = 2							-- 陆军演习最大士气
 
 NDefines.NNationDesigner.POINTS_AVAILABLE = 1000                        -- 自建国点数
 
-NDefines.NReligion.MIN_HARMONIZE_DEVELOPMENT = 1                        -- 和谐宗教所需的最小发展度
-NDefines.NReligion.YEARLY_HARMONIZATION_PROGRESS = 100                  -- 每年和谐进度
+--NDefines.NReligion.MIN_HARMONIZE_DEVELOPMENT = 1                        -- 和谐宗教所需的最小发展度
+--NDefines.NReligion.YEARLY_HARMONIZATION_PROGRESS = 100                  -- 每年和谐进度
