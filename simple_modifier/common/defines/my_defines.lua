e1bbf6a2971919a0511afb3118296ace08e95c1b
LinRN <44719485+Linrnn@users.noreply.github.com>
2024-11-01 23:02:36 +0800

    增加分离主义

--- simple_modifier/common/defines/my_defines.lua
+++ simple_modifier/common/defines/my_defines.lua
@@ -1,24 +1,39 @@
 NDefines.NCountry.CULTURE_MIN_DEVELOPMENT_TO_PROMOTE = 1                -- 接受文化所需的最小发展度
 NDefines.NCountry.NAT_FOCUS_YEARS = 1						            -- 改变国家焦点的冷却时间（年）
-NDefines.NCountry.POWER_MAX = 10000                                     -- 最大点数
 NDefines.NCountry.CONCENTRATE_DEVELOPMENT_DEVELOPMENT_DECREASE = 0      -- 集中发展度概率
 NDefines.NCountry.PILLAGE_CAPITAL_DEVELOPMENT_DECREASE = 0              -- 掠夺首都概率
+--NDefines.NCountry.POWER_MAX = 10000                                   -- 最大点数
 --NDefines.NCountry.BASE_POWER_INCREASE = 0                             -- 基础点数
 --NDefines.NCountry.REVOLT_SIZE_BASE = 0                                -- 基础叛军
 --NDefines.NCountry.RAZE_PROVINCE_COOLDOWN_YRS = 0                      -- 剥削发展度冷却时间（年）
 --NDefines.NCountry.EXPLOIT_COOLDOWN_MONTHS = 0                         -- 游牧烧地（月）
 --NDefines.NCountry.TRIBE_STARTING_DEV = 0                              -- 最低部落发展度
 
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_NO_CB_MULT = 0.5, -- AI desire for a province is multiplied by this if it doesn't have a valid cb for it (only used when annexing, not applied to cores)
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_CORE_MULT = 3.0, -- AI desire for a province is multiplied by this if it has a core on it
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_WARGOAL_MULT = 2.0, -- AI desire for a province is multiplied by this if it is the wargoal
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_CLAIM_MULT = 2.0, -- AI desire for a province is multiplied by this if it has a claim on it
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_NOT_CULTURE_MULT = 0.75, -- AI desire for a province is multiplied by this if it is not the same culture
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_VASSAL_MULT = 0.75, -- AI desire for a province is multiplied by this if it would go to their vassal instead of themselves
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_REAL_ADJACENT_MULT = 0.5, -- AI desire for a province is increased by this multiplier for each owned adjacent province
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_NOT_ADJACENT_MULT = 0.5, -- AI desire for a province is multiplied by this if it is not adjacent at all (including vassals and other provinces being taken in peace)
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_NO_INTEREST_MULT = 0, -- AI desire for a province is multiplied by this if it is not on their conquest list
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_OVEREXTENSION_MIN_MULT = 0.75, -- AI desire for a province is multiplied by this if it has 99% overextension (not applied to cores)
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_OVEREXTENSION_MAX_MULT = 1.5, -- AI desire for a province is multiplied by this if it has 0% overextension (not applied to cores)
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_ISOLATED_CAPITAL_MULT = 0.9, -- AI desire for a province if it is capital (costs a bit more to take)
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_ALLY_MULT = 0.5, -- AI desire for giving (non-core) provinces to its allies
+-- NDefines.NAI.PEACE_TERMS_PROVINCE_IMPORTANT_ALLY_MULT = 2, -- AI desire for giving provinces to allies that it has promised land
+
 --NDefines.NEconomy.MAX_BUILDING_SLOTS = 16                             -- 建筑物槽的最大数量，即可容纳的最大建筑物数量
 
 --NDefines.NMilitary.ARMY_DRILL_MAX_MORALE = 2							-- 陆军演习最大士气
 
---NDefines.DEVELOPMENT_CAP_BASE = 3                                       -- AI不会发展比这个或DEVELOPMENT_CAP_MULT*原始发展度更多的省份（以较大者为准）
+--NDefines.DEVELOPMENT_CAP_BASE = 3                                     -- AI不会发展比这个或DEVELOPMENT_CAP_MULT*原始发展度更多的省份（以较大者为准）
 --NDefines.DEVELOPMENT_CAP_MULT = 1
 
 NDefines.NNationDesigner.POINTS_AVAILABLE = 2000                        -- 自建国点数
 
-NDefines.NReligion.MAX_PAPAL_INFLUENCE = 1000                           -- 教廷影响力上限，天主教，控制台代码：add_pi
---NDefines.NReligion.MAX_CHURCH_POWER = 1000                            -- 教会力量/信仰力量上限，新教/圣公宗/胡斯派/犹太教，控制台代码：church_power
+--NDefines.NReligion.MAX_PAPAL_INFLUENCE = 1000                         -- 教廷影响力上限，天主教，控制台代码：add_pi
+--NDefines.NReligion.MAX_CHURCH_POWER = 10000                           -- 教会力量/信仰力量上限，新教/圣公宗/胡斯派/犹太教，控制台代码：church_power
 --NDefines.NReligion.MIN_HARMONIZE_DEVELOPMENT = 1                      -- 和谐宗教所需的最小发展度
 --NDefines.NReligion.YEARLY_HARMONIZATION_PROGRESS = 100                -- 每年和谐进度
